9f780f432565346b6d1f01935580e9dcf1b49dc9
krzysztof1x1 <krzysztoftomczyk2007@gmail.com>
2025-05-20 08:41:02 +0200

    pierwowzór strony oferta

---
+++ offer.php
@@ -0,0 +1,33 @@
+<?php
+require __DIR__ . '/includes/header.php';
+?>
+
+<div class="container">
+    <div class="form-box">
+        <h1>Nasza oferta</h1>
+        
+        <div class="offer-section">
+            <div class="offer-card">
+                <h2>Sala X</h2>
+                <ul>
+                    <li>Pojemność: 50 osób</li>
+                    <li>Profesjonalne nagłośnienie</li>
+                    <li>Cena: 2000 zł/8h</li>
+                </ul>
+            </div>
+            
+            <div class="offer-card">
+                <h2>Sala Y</h2>
+                <ul>
+                    <li>Pojemność: 100 osób</li>
+                    <li>VIP Lounge</li>
+                    <li>Cena: 3500 zł/8h</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+require __DIR__ . '/includes/footer.php';
+?>
